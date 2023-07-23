e5fdedd851cc4797106eaa13c6f82c87bf3de986
Tao Lin <2305764123@qq.com>
2023-07-23 10:15:04 +0800

    one

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false,
+  //配置@别名
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'components': '@/components',
+        'pages': '@/pages',
+        'utils': '@/utils',
+        'store': '@/store',
+        'router': '@/router',
+        'style': '@/style',
+      }
+    }
+  },
+})
